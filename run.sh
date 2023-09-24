fe0b739b203197aaf07fc51cfb72c6c7f3844d0f
JacobHA <jacobhadamczyk@gmail.com>
2023-09-24 13:54:22 -0400

    update runner

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=FA_clipping
-#SBATCH --time=23:00:00
-#SBATCH --mem-per-cpu=8gb
-#SBATCH --cpus-per-task=4
+#SBATCH --job-name=u-chi
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=1gb
+#SBATCH --cpus-per-task=12
 
 # Set filenames for stdout and stderr.  %j can be used for the jobid.
 # see "filename patterns" section of the sbatch man page for
 # additional options
 #SBATCH --error=outfiles/%j.err
 #SBATCH --output=outfiles/%j.out
-##SBATCH --partition=AMD6276
+#SBATCH --partition=AMD6276
 
-##SBATCH --array=1-5
+#SBATCH --array=1-5
 echo "using scavenger"
 
 # Prepare conda:
@@ -23,7 +23,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib
 echo "Start Run"
 echo `date`
 # python uchi_agent_NN.py
-python uchi_agent_MF.py
+python wandb_job.py
 
 # Diagnostic/Logging Information
 echo "Finish Run"
