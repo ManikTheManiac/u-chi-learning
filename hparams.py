6091a49bf3869d18e17323a3f55bf3118e0318eb
JacobHA <jacobhadamczyk@gmail.com>
2023-09-23 21:26:40 -0400

    new hparams based on re-interp. of parallel coords

--- hparams.py
+++ hparams.py
@@ -1,10 +1,10 @@
 cartpole_hparams = {
-    'beta': 2.1,
-    'batch_size': 1000,
-    'buffer_size': 20000,
-    'gradient_steps': 9,
-    'learning_rate': 0.01,
-    'target_update_interval': 100,
-    'tau': 0.9,
-    'tau_theta': 0.0009,
+    'beta': 1,
+    'batch_size': 400,
+    'buffer_size': 150000,
+    'gradient_steps': 20,
+    'learning_rate': 5.5e-3,
+    'target_update_interval': 70,
+    'tau': 0.85,
+    'tau_theta': 0.85,
 }
