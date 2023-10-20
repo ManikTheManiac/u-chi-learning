7b51f1eb9676f6ad90bd7fcd22ba9dd9dcb08115
JacobHA <jacobhadamczyk@gmail.com>
2023-10-20 14:34:11 -0400

    gaussian policy

--- darer/Models.py
+++ darer/Models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.distributions import Categorical
-from stable_baselines3.common.preprocessing import preprocess_obs, get_action_dim
+from stable_baselines3.common.preprocessing import preprocess_obs, get_action_dim, get_flattened_obs_dim, get_obs_shape
 import numpy as np
 from stable_baselines3.common.utils import zip_strict
 from gymnasium import spaces
@@ -15,7 +15,7 @@ class LogUNet(nn.Module):
         if isinstance(env.observation_space, spaces.Discrete):
             self.nS = env.observation_space.n
         elif isinstance(env.observation_space, spaces.Box):
-            self.nS = env.observation_space.shape[0]
+            self.nS = env.observation_space.shape[0]       
         self.nA = env.action_space.n
         self.fc1 = nn.Linear(self.nS, hidden_dim, device=self.device)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim, device=self.device)
@@ -195,11 +195,7 @@ class LogUsa(nn.Module):
         super(LogUsa, self).__init__()
         self.env = env
         self.device = device
-        if isinstance(env.observation_space, gym.spaces.Discrete):
-            self.nS = env.observation_space.n
-        elif isinstance(env.observation_space, gym.spaces.Box):
-            self.nS = env.observation_space.shape[0]
-
+        self.nS = get_flattened_obs_dim(self.env.observation_space)
         self.nA = get_action_dim(self.env.action_space)
         self.fc1 = nn.Linear(self.nS + self.nA, hidden_dim, device=self.device)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim, device=self.device)
@@ -218,3 +214,79 @@ class LogUsa(nn.Module):
         x = self.fc3(x)
         return x
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+LOG_SIG_MAX = 2
+LOG_SIG_MIN = -20
+epsilon = 1e-6
+
+epsilon = 1e-6
+
+# Initialize Policy weights
+def weights_init_(m):
+    if isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight, gain=1)
+        torch.nn.init.constant_(m.bias, 0)
+
+class GaussianPolicy(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_dim, observation_space, action_space):
+        super(GaussianPolicy, self).__init__()
+        
+        self.linear1 = nn.Linear(num_inputs, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+
+        self.mean_linear = nn.Linear(hidden_dim, num_actions)
+        self.log_std_linear = nn.Linear(hidden_dim, num_actions)
+
+        self.apply(weights_init_)
+
+        # action rescaling
+        if action_space is None:
+            self.action_scale = torch.tensor(1.)
+            self.action_bias = torch.tensor(0.)
+        else:
+            self.action_scale = torch.FloatTensor(
+                (action_space.high - action_space.low) / 2.)
+            self.action_bias = torch.FloatTensor(
+                (action_space.high + action_space.low) / 2.)
+            
+        self.observation_space = observation_space
+
+    def forward(self, obs):
+        obs = torch.Tensor(obs)#.to(self.device)
+        obs = preprocess_obs(obs, self.observation_space)
+        x = F.relu(self.linear1(obs))
+        x = F.relu(self.linear2(x))
+        mean = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, min=LOG_SIG_MIN, max=LOG_SIG_MAX)
+        return mean, log_std
+
+    def sample(self, state):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        normal = Normal(mean, std)
+        x_t = normal.rsample()  # for reparameterization trick (mean + std * N(0,1))
+        y_t = torch.tanh(x_t)
+        action = y_t * self.action_scale + self.action_bias
+        log_prob = normal.log_prob(x_t)
+        # Enforcing Action Bound
+        log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + epsilon)
+        # print(state)
+        # print(log_prob)
+        log_prob = log_prob.sum(-1, keepdim=True)
+        mean = torch.tanh(mean) * self.action_scale + self.action_bias
+        # print(mean, action, log_prob)
+        return action, log_prob, mean
+
+    def to(self, device):
+        self.action_scale = self.action_scale.to(device)
+        self.action_bias = self.action_bias.to(device)
+        return super(GaussianPolicy, self).to(device)
+    
+
+if __name__ == '__main__':
+    x= LogUNet(gym.make('CartPole-v1'))
