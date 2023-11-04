49e9a47679ebaed270aff7a0b229a126004fbf85
JacobHA <jacobhadamczyk@gmail.com>
2023-11-04 11:25:15 -0400

    new job

--- wandb_job.py
+++ wandb_job.py
@@ -6,6 +6,7 @@ from new_logac import LogUActor
 # env_id = 'CartPole-v1'
 # env_id = 'MountainCar-v0'
 env_id = 'LunarLander-v2'
+# env_id = 'PongNoFrameskip-v4'
 # env_id = 'HalfCheetah-v4'
 # env_id = 'Pendulum-v1'
 
@@ -14,10 +15,10 @@ def runner(config=None, run=None, device='cpu'):
     # Convert the necessary kwargs to ints:
     for int_kwarg in ['batch_size', 'target_update_interval', 'theta_update_interval']:
         config[int_kwarg] = int(config[int_kwarg])
-    config['buffer_size'] = 50_000
+    config['buffer_size'] = 200_000
     config['gradient_steps'] = 1
     config['train_freq'] = 1
-    config['learning_starts'] = 10_000
+    config['learning_starts'] = 5_000
 
     config.pop('actor_learning_rate')
     runs_per_hparam = 2
@@ -25,9 +26,9 @@ def runner(config=None, run=None, device='cpu'):
     wandb.log({'env_id': env_id})
 
     for _ in range(runs_per_hparam):
-        model = LogULearner(env_id, **config, log_interval=1500, use_wandb=True,
+        model = LogULearner(env_id, **config, log_interval=1000, use_wandb=True,
                             device=device, render=0)
-        model.learn(total_timesteps=50_000)
+        model.learn(total_timesteps=200_000)
         auc += model.eval_auc
     auc /= runs_per_hparam
     wandb.log({'avg_eval_auc': auc})
@@ -43,8 +44,8 @@ def wandb_agent():
 if __name__ == "__main__":
     # set up wandb variables (TODO: these should be set up as globals per user):
     entity = "jacobhadamczyk"
-    project="LogU-Cartpole"
-    sweep_id="rbtzmhyx"
+    project = "LogU-Cartpole"
+    sweep_id = "rbtzmhyx"
     # Parse the "algo" argument
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--device", type=str, default='cpu')
